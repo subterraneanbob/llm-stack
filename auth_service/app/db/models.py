436d1beee5510a3e42b613e3b5f52fa30387507c
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2026-04-17 12:47:38 +0300

    Remove redundant import

--- auth_service/app/db/models.py
+++ auth_service/app/db/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import DateTime, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
-from auth_service.app.db.base import Base
+from app.db.base import Base
 
 
 class User(Base):
