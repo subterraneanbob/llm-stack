90b9a74e1ae902e520744fd552066ea12de1270b
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2026-04-17 23:50:35 +0300

    Add a lifespan function to release resources on shutdown

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,8 +1,29 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.db.session import engine
+from app.db.base import Base
 
-app = FastAPI(title=settings.app_name)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Высвобождает ресурсы приложения.
+
+    Args:
+        app (FastAPI): Приложение FastAPI.
+    """
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+
+    yield
+
+    await engine.dispose()
+
+
+app = FastAPI(title=settings.app_name, lifespan=lifespan)
 
 
 @app.get("/health", tags=["system"])
