29c190571b4ffdf152a56c55f239de98c5d6f470
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2026-04-22 15:15:14 +0300

    Split large responses into chunks to fit Telegram message size limit

--- bot_service/app/bot/llm_response_relay.py
+++ bot_service/app/bot/llm_response_relay.py
@@ -5,10 +5,39 @@ from redis.asyncio import Redis
 
 from app.infra.redis import LLM_RESPONSE_READY_CH, llm_response_key
 
+MESSAGE_SIZE_LIMIT = 4096
+
+
+def split_text(text: str, limit: int = MESSAGE_SIZE_LIMIT) -> list[str]:
+    """
+    Разбивает текст на части длиной не более `limit` символов.
+    Разбивка производится по символу переноса строки, если он не найден, то просто
+    берётся первые `limit` символов.
+
+    Args:
+        text (str): Исходный текст.
+        limit (int, optional): Максимальный размер одной части.
+
+    Returns:
+        list[str]: Список частей, которые получились при разбивке исходного текста.
+    """
+    parts = []
+    while len(text) > limit:
+        split_at = text.rfind("\n", 0, limit)
+        if split_at == -1:
+            split_at = limit
+
+        parts.append(text[:split_at].strip())
+        text = text[split_at:].strip()
+
+    parts.append(text)
+    return parts
+
 
 class LLMResponseRelay:
     """
-    Класс, который отправляет ответы от LLM обратно пользователю Telegram.
+    Класс, который получает уведомления о поступлении нового ответа LLM
+    и отправляет их пользователю Telegram.
     """
 
     def __init__(self, redis: Redis):
@@ -51,10 +80,13 @@ class LLMResponseRelay:
                     tg_chat_id = message["data"]
                     response_key = llm_response_key(tg_chat_id)
 
-                    # Проверяем, что сообщение не пустое, отправляем ответ пользователю
-                    # и помечаем ключ Redis на удаление.
+                    # Проверяем, что сообщение не пустое, разбиваем ответ LLM на части
+                    # (у Telegram есть ограничение на длину сообщения),
+                    # отправляем пользователю и помечаем ключ Redis на удаление.
                     if (llm_response := await self.redis.get(response_key)) is not None:
-                        await bot.send_message(tg_chat_id, llm_response)
+                        for text_part in split_text(llm_response):
+                            await bot.send_message(tg_chat_id, text_part)
+
                         await self.redis.unlink(response_key)
         except asyncio.CancelledError:
             await pubsub.unsubscribe(LLM_RESPONSE_READY_CH)
