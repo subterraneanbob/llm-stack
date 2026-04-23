e2eefeb5ff7667e6aa519f8a5fffc8cc386f5075
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2026-04-23 13:39:03 +0300

    Refactor JWT tests

--- bot_service/tests/unit/test_security.py
+++ bot_service/tests/unit/test_security.py
@@ -4,12 +4,14 @@ from app.core.jwt import decode_and_validate
 from app.core.errors import InvalidTokenError, TokenExpiredError
 
 
-def test_decode_and_validate(access_token):
-    # Проверяем, что можно получить поля из токена
-    claims = decode_and_validate(access_token)
+def test_decode_and_validate(access_token_with_claims):
+    # Проверяем, что токен корретно проверяется, и что можно получить поля из токена
+    access_token, claims = access_token_with_claims
+    payload = decode_and_validate(access_token)
 
-    assert claims.get("sub")
-    assert claims.get("exp")
+    assert payload.get("sub")
+    assert payload.get("sub") == claims.get("sub")
+    assert payload == claims
 
 
 def test_expired_token(expired_access_token):
@@ -18,10 +20,7 @@ def test_expired_token(expired_access_token):
         decode_and_validate(expired_access_token)
 
 
-@pytest.mark.parametrize(
-    "wannabe_access_token", ["", "definitely-an-access-token", "abcdef123456"]
-)
-def test_invalid_token(wannabe_access_token):
+def test_invalid_token(invalid_access_token):
     # Неверные значения токена вызывают ошибку
     with pytest.raises(InvalidTokenError):
-        decode_and_validate(wannabe_access_token)
+        decode_and_validate(invalid_access_token)
